91020a16ccd2e8d096ec1b462e0271d608024c8e
Allure <samira-h@hotmail.co.uk>
2022-01-09 00:03:22 +0000

    password generator

---
+++ README.md
@@ -0,0 +1,25 @@
+# generatePasswordJS
+The motivation behind this very first JavaScript file is so that the webpage becomes interactive with the user.
+
+Having given a starter code with HTML and CSS meant that the display and layout were visable on the webapage. However, in order to make the service interactive it requires an addition file which is where JavaScript comes into play. 
+
+The more I practised whilst updating the webspage go live, I could understand certain concepts in which was hard to understand just by theory. 
+
+Unfortnatly, I was able to get the box with the questions and its 'returns' on cosole but just not on the actual webpage. 
+
+Installation
+To access this project on GitHub you can follow these links:
+
+GitHub repository: git@github.com:samira0101/AddvancedCSSPortfolio.git
+
+GitHub url:
+
+Refrences
+
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+
+https://www.w3schools.com/jsref/jsref_concat_string.asp
+
+https://dev.to/code_mystery/random-password-generator-using-javascript-6a
--- script.js
+++ script.js
@@ -1,54 +1,158 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var user;
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// define the function here
-function generatePassword () {
-  console.log ("you pressed the button!")
+var numberChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// // 4. display password to the page
-//   return "you created a password!"
-}
-
-
-
-
-
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function writePassword() {
+
+    // generate password
+
+    function randomGenerator(maxNum) {
+
+        return Math.floor(Math.random() * maxNum);
+
+    }
+
+    function generatePassword() {
+
+        let lowerCase;
+
+        let upperCase;
+
+        let number;
+
+        let special;
+
+        let pswLength;
+
+        length = (prompt("The length of your password characters: enter a number between 8 and 128"));
+
+        if ((length < 8) || (length > 128)) {
+
+            alert("password lenght invalid, please try again");
+
+            generatePassword();
+
+        } else if ((!length < 8) || (!length > 128)) {
+
+            pswLength = length;
+
+            alert("valid password");
+
+        }
+
+        if (confirm("Do you wan't to include lowercase in your password? Press 'ok' for yes and 'cancel' for no") == true) {
+
+        lowerCase = true;
+
+        } else {
+
+        lowerCase = false;
+
+        }
+
+        if (confirm("Do you wan't to include uppercase in your password? Press 'ok' for yes and 'cancel' for no") == true) {
+
+        upperCase = true;
+
+        } else {
+
+        upperCase = false;
+
+        }
+
+        if (confirm("Do you wan't to include number(s) in your password? Press 'ok' for yes and 'cancel' for no") == true) {
+
+        number = true;
+
+        } else {
+
+        number = false;
+
+        }
+
+        if (confirm("Do you wan't to include special characters in your password? Press 'ok' for yes and 'cancel' for no") == true) {
+
+        special = true;
+
+        } else {
+
+        special = false;
+        }
+
+        var allCharacters = [];
+
+        let generatedOutcome = "";
+
+        if (upperCase) {
+
+            allCharacters = allCharacters.concat(upperCaseChar);
+
+            var outcome = randomGenerator(upperCaseChar.length);
+
+            generatedOutcome += upperCaseChar[outcome];
+
+        }
+
+        if (lowerCase) {
+
+            allCharacters = allCharacters.concat(lowerCaseChar);
+
+            var outcome = randomGenerator(lowerCaseChar.length);
+
+            generatedOutcome += lowerCaseChar[outcome];
+
+        }
+
+        if (number) {
+
+            allCharacters = allCharacters.concat(numberChar);
+
+            var outcome = randomGenerator(numberChar.length);
+
+            generatedOutcome += numberChar[outcome];
+
+        }
+
+        if (special) {
+
+            allCharacters = allCharacters.concat(specialChar);
+
+            var outcome = randomGenerator(specialChar.length);
+
+            generatedOutcome += specialChar[outcome];
+
+        }
+
+        for (var i = 0; i < parseInt(pswLength); i++) {
+
+            var outcome = randomGenerator(lowerCaseChar.length);
+
+            generatedOutcome += allCharacters[i];
+
+        }
+
+      return generatedOutcome;
+
+    }
+
+    let password = generatePassword();
+
+    var passwordText = document.querySelector("#password");
+    
+
+    passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// generate password
-function generatePassword() {
-// ask for user password
-length = (prompt ("The length of your password characters: enter a number between 8 and 128"));
-
-if ((length  <8) || (length >128 )) {
-  alert ("password lenght invalid, please try again")
-  
-} else if ((!length  <8) || (!length >128 )) {
-  alert ("valid password")
-} else {
-  lowerCase = (prompt ("Do you want your password to contain a lowercase?"));
-  upperCase = (prompt ("Do you want your password to contain a uppercase?"));
-  number = (prompt ("Do you want to include a number in your password?"));
-  special = (prompt ("Do you want your password to contain a special character?"));
-};
-  special = (prompt ("Do you want your password to contain a special character?"));
-};
+generateBtn.addEventListener("click", writePassword)
